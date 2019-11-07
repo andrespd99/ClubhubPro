d3fe1cd3cf05ce77f4a659efb1107b100dd0fbe4
Andres Eloy <andreseloy@Cesars-MacBook-Pro.local>
2019-11-07 10:58:45 -0400

    Arreglando el peo

--- lib/models/news/News.dart
+++ lib/models/news/News.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:clubhub/UtilWidgets.dart';
 import 'package:clubhub/assets/colors.dart';
 import 'package:clubhub/models/news/ArticleDetail.dart';
 import 'package:flutter/cupertino.dart';
@@ -32,7 +33,7 @@ class _NewsState extends State<News> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {    
     _navigateToDetail(DocumentSnapshot article) {
       Navigator.push(
           context,
@@ -45,18 +46,7 @@ class _NewsState extends State<News> {
           future: _data,
           builder: (_, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(),
-                    Text("Cargando...",
-                        style: TextStyle(
-                            color: kClubhubBlueDark,
-                            fontFamily: 'Poppins-Medium'))
-                  ],
-                ),
-              );
+              return showLoadingCircle();
             } else {
               return ListView.builder(
                   itemCount: snapshot.data.length,
@@ -81,94 +71,76 @@ Widget _buildListItem(BuildContext context, DocumentSnapshot article) {
 
   return Column(
     children: <Widget>[
-      Padding(
-        padding: EdgeInsets.fromLTRB(7.0, 0, 0, 5.0),
-        child: Hero(
-          tag: article.data['title'],
-          child: Text(
-            article.data['title'],
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              fontFamily: 'Poppins-Medium',
-              fontSize: ScreenUtil().setSp(40),
-              color: kClubhubBlueDark,
-            ),
-          ),
-        ),
-      ),
-      Stack(
-        children: <Widget>[
-          Hero(
-            tag: article.documentID,
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color(0xfff2f2f2),
-              ),
-              width: MediaQuery.of(context).size.width,
-              height: 300,
-              child: FittedBox(
-                child: CachedNetworkImage(
-                  imageUrl: article.data['thumbnailUrl'], 
-                  placeholder: (context, url) => CircularProgressIndicator(),
-                  //                 FadeInImage.assetNetwork(
-                  //   placeholder: 'lib/assets/images/loading.gif',
-                  //   image: article.data['thumbnailUrl'],
-                  // ),
-                ),
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 20.0,
-            right: 15.0,
-            child: ClipOval(
-              child: Container(
-                // width: ScreenUtil().setWidth(90),
-                // height: ScreenUtil().setHeight(90),
-                width: 44.0,
-                height: 44.0,
-                color: Colors.white,
-                child: InkWell(
-                  child: _bookmarkIconState,
-                  onTap: () {
-                    _bookmarkIconStateChanger();
-                  },
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
+      showArticleTitle(article),
+      showArticleImage(article, context),
       SizedBox(
         height: ScreenUtil().setHeight(50),
       ),
       Divider()
     ],
   );
+}
 
-  // return Padding(
-  //   key: ValueKey(record.title),
-  //   padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-  //   child: Container(
-  //     decoration: BoxDecoration(
-  //       border: Border.all(color: Colors.grey),
-  //       borderRadius: BorderRadius.circular(5.0),
-  //     ),
-  //     child: ListTile(
-  //       title: Stack(
-  //         children: <Widget>[
-  //           Container(
-  //             child: Image(
-  //               image: NetworkImage(record.thumbnailUrl),
-  //               fit: BoxFit.fitWidth,
-  //             ),
-  //           ),
-  //           Text(record.title),
-  //         ],
-  //       ),
-  //       onTap: () => print(record),
-  //     ),
-  //   ),
-  // );
+Widget showArticleImage(DocumentSnapshot article, BuildContext context) {
+  return Stack(
+    children: <Widget>[
+      Hero(
+        tag: article.documentID,
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xfff2f2f2),
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: 300,
+          child: FittedBox(
+            child: CachedNetworkImage(
+              imageUrl: article.data['thumbnailUrl'],
+              placeholder: (context, url) => CircularProgressIndicator(),
+              //                 FadeInImage.assetNetwork(
+              //   placeholder: 'lib/assets/images/loading.gif',
+              //   image: article.data['thumbnailUrl'],
+              // ),
+            ),
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+      ),
+      // Positioned(
+      //   bottom: 20.0,
+      //   right: 15.0,
+      //   child: ClipOval(
+      //     child: Container(
+      //       // width: ScreenUtil().setWidth(90),
+      //       // height: ScreenUtil().setHeight(90),
+      //       width: 44.0,
+      //       height: 44.0,
+      //       color: Colors.white,
+      //       child: InkWell(
+      //         child: _bookmarkIconState,
+      //         onTap: () {
+      //           //_bookmarkIconStateChanger();
+      //         },
+      //       ),
+      //     ),
+      //   ),
+      // )
+    ],
+  );
+}
+
+Widget showArticleTitle(DocumentSnapshot article) {
+  return Padding(
+    padding: EdgeInsets.fromLTRB(7.0, 0, 0, 5.0),
+    child: Hero(
+      tag: article.data['title'],
+      child: Text(
+        article.data['title'],
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          //fontSize: ScreenUtil().setSp(40),
+          color: kClubhubBlueDark,
+        ),
+      ),
+    ),
+  );
 }
